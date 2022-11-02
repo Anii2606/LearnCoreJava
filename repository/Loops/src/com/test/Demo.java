a3662990df8c7fb27d5667b30c672a08b33e64cf
Anikesh Chouhan <115870421+Anii2606@users.noreply.github.com>
2022-11-02 09:29:46 +0530

    Add files via upload

---
+++ repository/Loops/src/com/test/Demo.java
@@ -0,0 +1,34 @@
+/*
+ * This is demo class to test package
+ * @author: Anikesh Chouhan
+ * @Date: 02nov
+ */
+
+
+//creating a package
+package com.test;
+
+//declaring class
+public class Demo {
+
+	//declaring a instance variable
+	int myValue = 15;
+	
+	//method to print value
+	public void printValue()
+	{
+		System.out.println("Value is :"+myValue);
+		
+	}
+	
+	//static method to print message
+	public static  void printMessage()
+	{
+	System.out.println("Welcome");
+	}
+	
+	
+}
+
+//end of class
+
